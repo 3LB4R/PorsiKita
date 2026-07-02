15c7bb9e3ead7cb4c7e6b802c64cfd124b67ede1
uswatunhasn <147926639+uswatunhasn@users.noreply.github.com>
2026-07-02 23:27:31 +0800

    V.1 UNAA npm install

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
-const apiRoutes = require("./routes/api");
+const apiRoutes = require('./routes/api');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-const logger = require("./middleware/logger");
+const logger = require('./middleware/logger');
 app.use(logger); // Middleware aktif!
 // Database Connection
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.error("❌ DB Connection Error:", err));
+  .then(() => console.log('✅ MongoDB Connected'))
+  .catch((err) => console.error('❌ DB Connection Error:', err));
 
 // Routes
-app.use("/api", apiRoutes);
+app.use('/api', apiRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on port ${PORT}`),
+);
